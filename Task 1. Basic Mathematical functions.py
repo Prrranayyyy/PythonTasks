390b666f3b31c1c4cb40ef1713c6a49293f87441
Prrranayyyy <prrranayyyy@gmail.com>
2025-06-11 11:17:51 +0530

    Add files via upload

---
+++ Task 1. Basic Mathematical functions.py
@@ -0,0 +1,16 @@
+print('                       Basic Mathematical Operations')
+print('')
+c= float(input("Enter the first number: "))
+d= float(input("Enter the second number: "))
+
+a= c+d
+b= c-d
+e= c*d
+f= c/d
+print('')
+print("Addition: ",a)
+print("Subtraction: ",b)
+print("Multiplication: ",e)
+print("Division: ",f)
+print('')
+print("                      THANK YOU")
---
+++ Task 2. Personalised Greeting.py
@@ -0,0 +1,4 @@
+f= input("Enter first name: ")
+l= input("Enter last name: ")
+n= f+ " " + l
+print("Hello " + n + "! Welcome to the python program")
